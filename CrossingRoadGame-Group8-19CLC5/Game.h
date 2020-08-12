3184e9e3f0e2ca7024b893a8b133c4e1a14bbf70
VuongPhuc <phuc16102001@gmail.com>
2020-08-13 01:03:39 +0700

    Update interface for Game class

--- CrossingRoadGame-Group8-19CLC5/Game.h
+++ CrossingRoadGame-Group8-19CLC5/Game.h
@@ -23,7 +23,32 @@ private:
 public:
 	Game();
 	~Game();
-	void drawLogo(int x, int y);
+
+	void menu();
+	void load();
+	void save();
+	void gameOver();
+
+	int getMenuChoice();
+	string getNewPlayerName();
+
+	Player* getPlayer();
+	vector<Movable*> getListEnemy();
+
+	void updateMoving(char moving);
+	void updateMinRow();
+	void updateMovable();
+	void levelUp();
+	void createLevel();
+
+	void drawLogo(int x, int y);	   	
+	void drawMenu(int x, int y, int choice);
+	void draw();
+	void drawInfo();
+	void drawBorder(int color);
+	void drawLegend(int color);
+	void drawObject();
+	void drawScoreBoard();
 };
 
 #endif
