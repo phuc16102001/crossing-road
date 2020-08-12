d1fecc4d1bd1ea27bc61889aea61a5cb0cfbc53a
Kagvn <khang30012001@gmail.com>
2020-08-12 20:41:55 +0700

    Update Player.cpp

--- CrossingRoadGame-Group8-19CLC5/Player.cpp
+++ CrossingRoadGame-Group8-19CLC5/Player.cpp
@@ -2,27 +2,39 @@
 
 void Player::setName(string name) {
 	//Set name
+	this->name = name;
 }
 
 string Player::getName() {
 	//Get name
+	return name;
 }
 
 Player::Player() {
 	//Init name = "" and alive = false
+	name = "";
+	alive = false;
 }
 
 void Player::setAlive(bool alive) {
 	//Set alive
+	this->alive = alive;
 }
 
 bool Player::isTouch(vector<Movable*> listEnemy) {
 	//Check all movable*, each one check if the same row and abs(playerRow - objectRow)<3) then return true
 	//Otherwise, return false
+	for (int i = 0; i < listEnemy.size(); i++) {
+		if (listEnemy[i]->getRow() == getRow() && abs(getRow() - listEnemy[i]->getRow()) < 3) {
+			return true;
+		}
+	}
+	return false;
 }
 
 bool Player::isAlive() {
 	//Return alive
+	return alive;
 }
 
 vector<string> Player::getImgString() {
