ae9c6e03c4babf291940de2bcf53d47c5ba5182b
krislam2001 <63941511+krislam2001@users.noreply.github.com>
2020-08-12 21:52:45 +0700

    Update Car.cpp

--- CrossingRoadGame-Group8-19CLC5/Car.cpp
+++ CrossingRoadGame-Group8-19CLC5/Car.cpp
@@ -2,16 +2,22 @@
 
 Car::Car(int row, int col, Light* light) : Movable(row, col) {
 	//Assign light then add this into Light->add
+	light = &Light::Light(row);
+	this->light = light;
+	Light::add(this->light);
 }
 
 void Car::update() {
 	//If canMove then Movable::update()
+	if (canMove == true) Movable::update();
 }
 
 void Car::notify(bool canMove) {
 	//Assign this->canMove = canMove
+	this->canMove = canMove;
 }
 
 Car::~Car() {
 	//Call remove this from Light mediator
+	Light::remove;
 }
