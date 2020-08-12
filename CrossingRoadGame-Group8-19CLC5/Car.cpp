6742d868ad2d6dc153a7cb351dabcee5d7cef566
VuongPhuc <phuc16102001@gmail.com>
2020-08-12 23:31:08 +0700

    Rewrite car implementation

--- CrossingRoadGame-Group8-19CLC5/Car.cpp
+++ CrossingRoadGame-Group8-19CLC5/Car.cpp
@@ -1,23 +1,20 @@
 #include "Car.h"
 
 Car::Car(int row, int col, Light* light) : Movable(row, col) {
-	//Assign light then add this into Light->add
-	light = &Light::Light(row);
 	this->light = light;
-	Light::add(this->light);
+	light->add(this);
 }
 
 void Car::update() {
-	//If canMove then Movable::update()
-	if (canMove == true) Movable::update();
+	if (canMove == true) {
+		Movable::update();
+	}
 }
 
 void Car::notify(bool canMove) {
-	//Assign this->canMove = canMove
 	this->canMove = canMove;
 }
 
 Car::~Car() {
-	//Call remove this from Light mediator
-	Light::remove;
+	light->remove(this);
 }
