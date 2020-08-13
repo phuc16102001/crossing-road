b3fbc9225df84530602b1d2853f84d03a7a9c4c8
VuongPhuc <phuc16102001@gmail.com>
2020-08-13 11:33:57 +0700

    Write some function in Game.cpp Write interface for getMenuChoice

--- CrossingRoadGame-Group8-19CLC5/Game.cpp
+++ CrossingRoadGame-Group8-19CLC5/Game.cpp
@@ -6,7 +6,43 @@ Game::Game() {
 }
 
 Game::~Game() {
+	delete player;
+}
 
+void Game::menu() {
+	int choice = getMenuChoice();
+
+	//Nothing
+}
+
+int Game::getMenuChoice() {
+	//Clear screen and drawLogo at logoX logoY
+	//int choice = 0; initialize
+	//run until press enter {
+	//drawMenu(menuX,menuY,choice);
+	//char input = _getch(); <- convert to upper letter
+	//if W -> choice=(choice-1+maxMenuChoice)%maxMenuChoice	//Move up
+	//if S -> choice=(choice+1)%maxMenuChoice				//Move down
+	//clue: enter = char(13)
+	//}
+}
+
+Player* Game::getPlayer() {
+	return player;
+}
+
+void Game::drawMenu(int x, int y, int choice) {
+	string menu[maxMenuChoice] = { "1. Start game","2. Load game","3. Setting", "4. Information","5. Exit" };
+	for (int i = 0; i < maxMenuChoice; i++) {
+		console.gotoXY(x, y + i);
+		if (i == choice) {
+			console.setTextColor(colorGreen);
+		}
+		else {
+			console.setTextColor(colorWhite);
+		}
+		cout << menu[i];
+	}
 }
 
 void Game::drawLogo(int x, int y) {
--- CrossingRoadGame-Group8-19CLC5/Game.h
+++ CrossingRoadGame-Group8-19CLC5/Game.h
@@ -19,36 +19,36 @@ private:
 	int levelRow = 8;
 	int nObjects = 2;
 	int minRow = 0;
-	int score = 0;
+	int playerScore = 0;
 public:
 	Game();
 	~Game();
 
 	void menu();
-	void load();
-	void save();
-	void gameOver();
+	//void load();
+	//void save();
+	//void gameOver();
 
 	int getMenuChoice();
-	string getNewPlayerName();
+	//string getNewPlayerName();
 
 	Player* getPlayer();
-	vector<Movable*> getListEnemy();
+	//vector<Movable*> getListEnemy();
 
-	void updateMoving(char moving);
-	void updateMinRow();
-	void updateMovable();
-	void levelUp();
-	void createLevel();
+	//void updateMoving(char moving);
+	//void updateMinRow();
+	//void updateMovable();
+	//void levelUp();
+	//void createLevel();
 
 	void drawLogo(int x, int y);	   	
 	void drawMenu(int x, int y, int choice);
-	void draw();
-	void drawInfo();
-	void drawBorder(int color);
-	void drawLegend(int color);
-	void drawObject();
-	void drawScoreBoard();
+	//void draw();
+	//void drawInfo();
+	//void drawBorder(int color);
+	//void drawLegend(int color);
+	//void drawObject();
+	//void drawScoreBoard();
 };
 
 #endif
