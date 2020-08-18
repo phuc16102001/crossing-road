2bd5360792485300a42e7cb774c04c46d86c28fb
VuongPhuc <phuc16102001@gmail.com>
2020-08-19 01:08:09 +0700

    Change directory of InformationFile Write instruction for score

---
+++ CrossingRoadGame-Group8-19CLC5/Score.h
@@ -0,0 +1,14 @@
+/*
+
+Class Score
+attr: name (string), score (int)
+method: 
++ Score(name,score)
++ getName()
++ getScore();
+
+bool compareScore(const Score& scoreA, const Score& scoreB)
+return getScore of a > getScore of b;
+
+Implement in Cpp pls
+*/
